752ecc2058c0e2ef3a2c79731e9e8270b18ffbbd
tomo07 <ms03kt@yahoo.co.jp>
2015-10-24 19:59:51 +0000

    challenge220151025

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: {  maximum: 20 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
+    #年齢は数字以外入力できない、0歳以上
+    validates :age , numericality: {greater_than:0}, presence: true
 end
